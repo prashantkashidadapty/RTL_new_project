54e71799244fe334f20d1e9db7f09e00fe719387
Prashant Kashid <prashantkashidadapty@gmail.com>
2022-05-11 21:01:45 +0530

    fixed

--- Src/component/CustomButton.tsx
+++ Src/component/CustomButton.tsx
@@ -1,41 +1,38 @@
-import React from 'react'
-import { TouchableOpacity,View ,Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
 
-type Props={
+type Props = {
   title: string;
-  onPress:any;
-}
+  onPress: any;
+};
 
-const CustomButton: React.FC<Props> =(props: Props)=>{
+const CustomButton: React.FC<Props> = (props: Props) => {
   return (
-<TouchableOpacity style={styles.buttoncontainer} onPress={props.onPress}>
-  <View>
-<Text style={styles.buttontext}>{props.title}</Text>
-  </View>
-</TouchableOpacity>
-  )
-}
+    <TouchableOpacity style={styles.buttoncontainer} onPress={props.onPress}>
+      <View>
+        <Text style={styles.buttontext}>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-const styles= StyleSheet.create({
-  buttoncontainer:{
-    backgroundColor:'skyblue',
-    height:50,
-    width:'50%',
-    flexDirection:'row',
-alignSelf:'center',
-alignItems:'center',
-borderRadius:40,
-textAlign:'center'
+const styles = StyleSheet.create({
+  buttoncontainer: {
+    backgroundColor: 'skyblue',
+    height: 50,
+    width: '50%',
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    textAlign: 'center',
   },
-buttontext:{
-fontFamily:'times new roman',
-fontSize:20,
-textAlign:'center',
-color:'brown',
-padding:5,
-
-
-},
-
+  buttontext: {
+    fontFamily: 'times new roman',
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'brown',
+    padding: 5,
+  },
 });
 export default CustomButton;
